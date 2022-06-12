1e6aa0b18d51443f192568cbcf164e7084a01871
kdj9878 <kdj9878@gmail.com>
2022-06-12 17:32:45 +0900

    #feat
    - 콜라츠 추측

--- test/practice/CollatzConjecture.java
+++ test/practice/CollatzConjecture.java
@@ -10,19 +10,21 @@ public class CollatzConjecture {
 
     @ParameterizedTest
     @DisplayName("콜라츠 추측")
-    @ValueSource(ints = {8, 10, 6242342})
+    @ValueSource(ints = {626331})
     public void solution(int num) {
         int answer = 0;
 
-        long castedNum = (long) num;
+        // int로 할 경우 표현 범위를 넘어서기 때문에 long으로 형변환을 함
+        long castedNum = num;
 
         while(castedNum != 1){
-            if(castedNum%2 == 0){
-                castedNum = castedNum / 2;
-            } else {
+            if(castedNum%2 > 0){
                 castedNum = (castedNum * 3) + 1;
+            } else {
+                castedNum = castedNum / 2;
             }
             count++;
+
             if(count == 500) {
                 count = -1;
                 break;

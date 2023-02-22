5e2ddf1b57fc6c721bfe10a56ef8d2b51e36a655
kang.dj <kang.dj@ensecure.co.kr>
2023-02-22 13:19:49 +0900

    CreateBigNumber.java
    - 큰 수 만들기(StringBuffer 활용 실패)
    - 조합 풀이로는 불가
    - 다른 사람 풀이 추가

--- test/greedy/CreateBigNumber.java
+++ test/greedy/CreateBigNumber.java
@@ -89,7 +89,22 @@ public class CreateBigNumber {
     }
 
     @Test
-    void createBigNumberByCombination(){
+    void createBigNumberOther(){
+        StringBuilder answer = new StringBuilder("");
+        int len = number.length() - k;
+        int start = 0;
 
+        while(start < number.length() && answer.length() != len) {
+            int leftNum = k + answer.length() + 1;
+            int max = 0;
+            for (int j = start; j < leftNum; j++) {
+                if (max < number.charAt(j) - '0') {
+                    max = number.charAt(j) - '0';
+                    start = j + 1;
+                }
+            }
+            answer.append(Integer.toString(max));
+        }
+        System.out.println(answer.toString());
     }
 }
